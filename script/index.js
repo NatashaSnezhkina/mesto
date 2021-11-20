b68f36d9e6c9b1d23cf1b6a4ef319cba1b4443ef
Natasha Snezhkina <snezhkina.natasha@yandex.ru>
2021-11-20 19:14:06 +0300

    after first review

--- script/index.js
+++ script/index.js
@@ -1,33 +1,31 @@
-const EditButton = document.querySelector(".edit-button");
+const editButton = document.querySelector(".edit-button");
 const popup = document.querySelector(".popup");
-const CloseButton = document.querySelector(".close-button");
+const closeButton = document.querySelector(".close-button");
+const formElement = document.querySelector('.form');
+const nameInput = document.querySelector('.field_type_name');
+const jobInput = document.querySelector('.field_type_description');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const submitButton = document.querySelector(".submit-button");
 
 function open() {
-  popup.classList.add ("popup_opened");
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+  popup.classList.add("popup_opened");
 }
 
 function close() {
-  popup.classList.remove ("popup_opened");
+  popup.classList.remove("popup_opened");
 }
 
-EditButton.addEventListener ('click', open);
-CloseButton.addEventListener ('click', close);
-
-let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('.field_name');
-let jobInput = document.querySelector('.field_description');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-const SubmitButton = document.querySelector(".submit-button");
-
-nameInput.value = profileTitle.textContent;
-jobInput.value = profileSubtitle.textContent;
-
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
   close();
 }
 
-formElement.addEventListener ('submit', formSubmitHandler);
+editButton.addEventListener('click', open);
+closeButton.addEventListener('click', close);
+
+formElement.addEventListener('submit', formSubmitHandler);
