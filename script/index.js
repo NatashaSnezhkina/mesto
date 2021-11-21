b33f30fadb58eefed2cc5493ff18d68a0657d689
Natasha Snezhkina <snezhkina.natasha@yandex.ru>
2021-11-21 14:26:27 +0300

    after second review

--- script/index.js
+++ script/index.js
@@ -1,21 +1,21 @@
-const editButton = document.querySelector(".edit-button");
-const popup = document.querySelector(".popup");
-const closeButton = document.querySelector(".close-button");
+const editButton = document.querySelector('.edit-button');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.close-button');
 const formElement = document.querySelector('.form');
 const nameInput = document.querySelector('.field_type_name');
 const jobInput = document.querySelector('.field_type_description');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const submitButton = document.querySelector(".submit-button");
+const submitButton = document.querySelector('.submit-button');
 
 function open() {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 }
 
 function close() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
