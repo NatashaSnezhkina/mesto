d03df1e9396d3d595a50b05fbe0cdb4b8511e1b0
Natasha Snezhkina <snezhkina.natasha@yandex.ru>
2021-11-29 14:11:02 +0300

    cards

--- script/index.js
+++ script/index.js
@@ -1,31 +1,88 @@
 const editButton = document.querySelector('.edit-button');
-const popup = document.querySelector('.popup');
-const closeButton = document.querySelector('.close-button');
+const addButton = document.querySelector('.add-button');
+const popupEdit = document.querySelector('.popup-edit');
+const popupAdd = document.querySelector('.popup-add');
+const closeButtonPopupEdit = document.querySelector('.close-button_popup_edit');
+const closeButtonPopupAdd = document.querySelector('.close-button_popup_add');
 const formElement = document.querySelector('.form');
 const nameInput = document.querySelector('.field_type_name');
 const jobInput = document.querySelector('.field_type_description');
+const titleInput = document.querySelector('.field_type_title');
+const linkInput = document.querySelector('.field_type_link');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const submitButton = document.querySelector('.submit-button');
+const elementsContainer = document.querySelector('.elements');
+const templateEl = document.querySelector('.element-template');
 
-function open() {
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function open(popupType) {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
-  popup.classList.add('popup_opened');
-}
-
-function close() {
-  popup.classList.remove('popup_opened');
+  popupType.classList.add('popup_opened');
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  close();
+  close(popupEdit);
 }
 
-editButton.addEventListener('click', open);
-closeButton.addEventListener('click', close);
+function close(popupType) {
+  popupType.classList.remove('popup_opened');
+}
+
+function render() {
+  const html = initialCards.map((item, idx, arr) => {
+    return getCard(item);
+  });
+  elementsContainer.append(...html);
+}
+
+function getCard(item) { 
+  const newCard = templateEl.content.cloneNode(true);
+  const headerCard = newCard.querySelector('.element__title');
+  headerCard.textContent = item.name;
+
+  const photoCard = newCard.querySelector('.element__photo');
+  photoCard.src = item.link;
+
+  return newCard;
+}
+
+render();
+
+
+addButton.addEventListener('click', () => {open(popupAdd);});
+editButton.addEventListener('click', () => {open(popupEdit);});
+closeButtonPopupEdit.addEventListener('click', () => {close(popupEdit);});
+closeButtonPopupAdd.addEventListener('click', () => {close(popupAdd);});
 
 formElement.addEventListener('submit', formSubmitHandler);
